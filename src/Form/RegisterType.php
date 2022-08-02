158d10e47cfbccd99f0d2571859b5e9fa2b78fb1
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2022-08-02 18:29:12 +0200

    (ft) Added Register

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir une adresse email'
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe et la confirmation doivent être identiques',
+                'required' => true,
+                'label' => 'Votre mot de passe',
+                'first_options' => [ 'label' => 'Mot de passe'],
+                'second_options' => [ 'label' => 'Confirmez votre mot de passe'],
+
+                'attr' => [
+                    'placeholder' => 'Merci de saisir un mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire"
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
