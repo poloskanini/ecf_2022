54d8bb32c029b2f81e77a077dfcfe16298f77ccc
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2022-08-07 18:28:13 +0200

    (ft) navbar profile dropdown OK-2

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,7 +27,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(nullable: true)]
     private ?string $name = null;
 
     #[ORM\Column(nullable: true)]
