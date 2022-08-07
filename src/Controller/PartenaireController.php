3fcb6b3c131a253a4b7ad1021928571242a1c047
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2022-08-07 19:34:27 +0200

    Commit de fin de journée :)

--- src/Controller/PartenaireController.php
+++ src/Controller/PartenaireController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('admin/partenaires')]
+#[Route('/partenaires')]
 class PartenaireController extends AbstractController
 {
     #[Route('/', name: 'app_partenaire_index', methods: ['GET'])]
