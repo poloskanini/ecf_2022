fb87564e8952c06f3cf63e78b4fb023f42d89cf2
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2022-08-10 10:49:35 +0200

    Commit début de journée

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,7 +13,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('home'); // Redirection vers ma home (gérée par HomeController et affichée sur home/index.html.twig)
         }
 
         // get the login error if there is one
