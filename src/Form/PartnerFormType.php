8ed41a5c30ed2882b99f8b0f11df38716d663303
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2022-08-28 15:57:38 +0200

    🔥✨EDITION d'un partenaire OK git add . Thousands thx to Symfo's God Papoel...-3

--- src/Form/PartnerFormType.php
+++ src/Form/PartnerFormType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class PartnerFormType extends AbstractType
@@ -16,7 +17,9 @@ class PartnerFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du partenaire',
+            ])
             ->add('isPlanning', CheckboxType::class, [
                 'required' => false,
                 'label' => false,
